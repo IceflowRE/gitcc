600dbda9618d180548a59a18fdff1599d4f7bbca
Iceflower <iceflower@gmx.de>
2022-09-02 15:32:28 +0200

    [*] Fox fox

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {get_commits} from './commit'
 import {CommitValidator, Result} from './commit-validator'
-import {check_commits, get_validator, print_results} from './utility'
+import {check_commits, get_validator, download_validator_file, print_results} from './utility'
 
 async function run(): Promise<void> {
     try {
@@ -13,19 +13,13 @@ async function run(): Promise<void> {
 
         const validator_file: string = core.getInput('validator_file')
         const validator_name: string = core.getInput('validator')
-        const token: string = core.getInput('access_token')
+        const access_token: string = core.getInput('access_token')
         // just to be sure
-        core.setSecret(token)
+        core.setSecret(access_token)
 
-        const octokit = github.getOctokit(token)
-        const data = await octokit.rest.repos.getContent({
-            path: validator_file,
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            ref: github.context.payload['head_commit']['id'],
-        })
-        core.info(data.data.toString())
-
+        if (!await download_validator_file(validator_file, access_token)) {
+            return
+        }
         const validator: CommitValidator = await get_validator(validator_name, validator_file)
 
         const checks: Result[] = check_commits(get_commits(), validator)
--- typescript/src/utility.ts
+++ typescript/src/utility.ts
@@ -2,6 +2,8 @@ import {CommitValidator, Result, Status} from './commit-validator'
 import * as core from '@actions/core'
 import {Commit} from './commit'
 import {SimpleTag} from './validation'
+import * as github from "@actions/github";
+import * as fs from "fs";
 
 export async function get_validator(validator: string, validator_file: string): Promise<CommitValidator> {
     if (validator_file !== '' && validator !== '') {
@@ -12,7 +14,7 @@ export async function get_validator(validator: string, validator_file: string): 
     }
 
     if (validator_file !== '') {
-        return new (await import_validator_cls(validator_file))()
+        return new (await import_validator_cls("./validator.mjs"))()
     }
     switch (validator) {
         case 'SimpleTag':
@@ -61,4 +63,30 @@ export function check_commits(commits: Commit[], validator: CommitValidator): Re
         checks.push(validator.validate(commit))
     }
     return checks
+}
+
+// returns success
+export async function download_validator_file(validator_file: string, access_token: string): Promise<boolean> {
+    const octokit = github.getOctokit(access_token)
+    const response = await octokit.rest.repos.getContent({
+        path: validator_file,
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        ref: github.context.payload['head_commit']['id'],
+    })
+    if (response.status != 200) {
+        core.setFailed(`failed to retrieve validator file '${response.url}'`)
+        return false
+    }
+    if (!Array.isArray(response)) {
+        core.setFailed(`given path '${response.url}' was a directory`)
+        return false
+    }
+    if(!("content" in response.data)) {
+        core.setFailed(`download of '${response.url}' failed`)
+        return false
+    }
+    const buffer = Buffer.from(response.data.content, 'base64').toString('utf-8')
+    fs.writeFile("./validator.mjs", buffer, err => { if (err) throw err })
+    return true
 }
