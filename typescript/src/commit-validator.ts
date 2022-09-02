f2760352ac8cfeafe09617f8751c0ac747996866
Iceflower <iceflower@gmx.de>
2022-09-02 17:04:54 +0200

    [*] Fox fox

--- typescript/src/commit-validator.ts
+++ typescript/src/commit-validator.ts
@@ -17,8 +17,7 @@ export class Result {
         this.commit = commit
     }
 
-    // @ts-ignore
-    public toString(): string {
+    toString(): string {
         let msg: string = this.status.toString()
         if (this.commit !== undefined) {
             msg += ` | ${this.commit.hexsha} - ${this.commit.summary()}`
